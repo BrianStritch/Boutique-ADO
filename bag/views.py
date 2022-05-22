774eef5ab44d0c70d3b618e36a01be7947d71159
BrianStritch <brians12ca18@gmail.com>
2022-05-22 22:07:59 +0000

    added toasts and messages for relevant actions

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.contrib import messages
+from django.db.models import Q
+from products.models import Product, Category
 
 # Create your views here.
 
@@ -14,6 +17,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ add a quantity of the selected product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -34,6 +38,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'You have succesfully added {product.name} to the shopping bag.')
 
     request.session['bag'] = bag 
 

7ad5ce44178f034fe29749269f3cccbb3dc32a9c
BrianStritch <brians12ca18@gmail.com>
2022-05-31 22:13:14 +0000

    added checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -11,7 +11,7 @@ var style = {
       fontSize: '16px',
       fontSmoothing: 'antialiased',      
       '::placeholder': {
-        color: '#aab7c4',
+        color: '#87BBFD',
       },
     },
     invalid: {
@@ -21,6 +21,7 @@ var style = {
 }
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
 
 /* handle realtime validation errors on the card element */
 card.addEventListener('change', function(event) {
@@ -48,7 +49,18 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrf_token = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+      'csrfmiddlewaretoken': csrf_token,
+      'client_secret': clientSecret,
+      'saveInfo': saveInfo,
+    }
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function(){
+      stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
             billing_details: {
@@ -59,7 +71,7 @@ form.addEventListener('submit', function(ev) {
                 line1: $.trim(form.street_address1.value),
                 line2: $.trim(form.street_address2.value),
                 city: $.trim(form.town_or_city.value),
-                county: $.trim(form.county.value),
+                country: $.trim(form.country.value),
                 country: $.trim(form.country.value), 
               }
             }
@@ -71,29 +83,31 @@ form.addEventListener('submit', function(ev) {
             line1: $.trim(form.street_address1.value),
             line2: $.trim(form.street_address2.value),
             city: $.trim(form.town_or_city.value),
-            county: $.trim(form.county.value),
-            country: $.trim(form.country.value), 
-            postcode: $.trim(form.postcode.value), 
+            country: $.trim(form.country.value),
+            country: $.trim(form.country.value),
           }
         },
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit(); 
-                /* this is where i can call a function to update the stock quantity */
-            }
-        }
-    });
+      }).then(function(result) {
+          if (result.error) {
+              var errorDiv = document.getElementById('card-errors');
+              var html = `
+                  <span class="icon" role="alert">
+                  <i class="fas fa-times"></i>
+                  </span>
+                  <span>${result.error.message}</span>`;
+              $(errorDiv).html(html);
+              $('#payment-form').fadeToggle(100);
+              $('#loading-overlay').fadeToggle(100);
+              card.update({ 'disabled': false});
+              $('#submit-button').attr('disabled', false);
+          } else {
+              if (result.paymentIntent.status === 'succeeded') {
+                  form.submit();
+                  
+              }
+          }
+      });
+    }).fail(function(){
+      location.reload()
+    })    
 });

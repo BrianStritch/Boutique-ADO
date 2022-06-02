86185f8b69813db1ebb6c2a379444be00076786a
BrianStritch <brians12ca18@gmail.com>
2022-06-02 22:36:28 +0000

    added profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -3,8 +3,10 @@ from django.db.models import Sum
 from django.db import models
 from django.conf import settings
 from products.models import Product
+from profiles.models import UserProfile
 
 from django_countries.fields import CountryField
+
 
 class Order(models.Model):
 
@@ -12,6 +14,14 @@ class Order(models.Model):
         max_length=32,
         null=False,
         editable=False
+        )
+        
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
         )
 
     full_name = models.CharField(

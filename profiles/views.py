18422e3d632214e6ff775610437eb2923dc4889c
BrianStritch <brians12ca18@gmail.com>
2022-06-05 00:04:35 +0000

    update user profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -7,9 +8,13 @@ def profile(request):
     A view to display a users profile
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'orders': orders,
+        'form': form,
         }
 
     return render(request, template, context)
